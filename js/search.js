cbb7981300244636fcb97f342753b72d5d305075
JD Bothma <jbothma@gmail.com>
2017-02-06 19:45:38 +0200

    Add highlighting and stemming since first query does stemming

--- js/search.js
+++ js/search.js
@@ -11,15 +11,21 @@
   var error = function(jqXHR, textStatus, errorThrown) {
     console.log(textStatus, errorThrown, jqXHR);
   };
-  var success = function(data) {
-    for (i in data) {
-      var rows = "";
-      for (key in data[i]) {
-        rows += "<tr><td>" + key + "</td><td>" + data[i][key] + "</td></tr>";
-      }
-      $('<table class="table"><tr><th>field</th><th>value</th></tr>' + rows + '</table>').appendTo(resultsContainer);
-    };
-  }
+  var success = function(query) {
+    return function(data) {
+      for (i in data) {
+        var rows = "";
+        for (key in data[i]) {
+          rows += "<tr><td>" + key + "</td><td>" + data[i][key] + "</td></tr>";
+        }
+        $('<table class="table"><tr><th>field</th><th>value</th></tr>' + rows + '</table>').appendTo(resultsContainer);
+      };
+      resultsContainer.mark(stemmer(query), {
+        separateWordSearch: true,
+        accuracy: "complementary"
+      });
+    }
+  };
   var complete = function() {
     stopSpinner();
   }
@@ -28,7 +34,7 @@
     startSpinner();
     $.ajax("https://data.code4sa.org/resource/9vmn-5tnb.json?$q=" + searchbox.value, {
       error: error,
-      success: success
+      success: success(searchbox.value)
     })
     return false;
   };
